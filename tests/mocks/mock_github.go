2c7f12ec6a9d06672d63e6f5afe6ee52c36b3726
Jorge Baptista <jorgebaptista.dev@outlook.com>
2024-12-15 15:52:57 +0000

    Add mock methods for creating, deleting, and listing prs to reflect client interface

--- tests/mocks/mock_github.go
+++ tests/mocks/mock_github.go
@@ -7,8 +7,33 @@ import (
 )
 
 type MockGitHubClient struct {
-	Repos []*github.Repository // Mock repos
-	Err   error
+	Repos        []*github.Repository  // Mock repos
+	PullRequests []*github.PullRequest // Mock pull requests
+	Err          error
+}
+
+func (m *MockGitHubClient) CreateRepoForOwner(ctx context.Context, owner, repoName string) (*github.Repository, error) {
+	if m.Err != nil {
+		return nil, m.Err
+	}
+	repo := &github.Repository{Name: github.String(repoName)}
+	m.Repos = append(m.Repos, repo) // Add to mock repos
+	return repo, nil
+}
+
+func (m *MockGitHubClient) DeleteRepoForOwner(ctx context.Context, owner, repoName string) error {
+	if m.Err != nil {
+		return m.Err
+	}
+
+	for i, repo := range m.Repos {
+		if *repo.Name == repoName {
+			m.Repos = append(m.Repos[:i], m.Repos[i+1:]...)
+			break
+		}
+	}
+
+	return nil
 }
 
 func (m *MockGitHubClient) ListReposForOwner(ctx context.Context, owner string) ([]*github.Repository, error) {
@@ -16,4 +41,11 @@ func (m *MockGitHubClient) ListReposForOwner(ctx context.Context, owner string) 
 		return nil, m.Err
 	}
 	return m.Repos, nil
+}
+
+func (m *MockGitHubClient) ListPullRequestsForOwner(ctx context.Context, owner, repoName string) ([]*github.PullRequest, error) {
+	if m.Err != nil {
+		return nil, m.Err
+	}
+	return m.PullRequests, nil
 }
